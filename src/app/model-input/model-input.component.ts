ef90f9774cae896db2164f0871e44cb2db0b64d6
Ravi Mohan <ravi.mohan@roonga.com.au>
2024-08-18 13:25:29 +1000

    clean up model

--- src/app/model-input/model-input.component.ts
+++ src/app/model-input/model-input.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, inject, model, signal } from '@angular/core';
+import { Component, computed, effect, inject, model } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
